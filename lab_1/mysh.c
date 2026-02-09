c9a7a44274407d9bdea19f745bc83848f150f68a
choco-melk <hullezachristianjave@gmail.com>
2026-02-10 00:58:01 +0800

    feat: added parser that checks syntax of user input and puts values to a command struct

--- lab_1/mysh.c
+++ lab_1/mysh.c
@@ -4,18 +4,42 @@
 int main(){
     char input[MAX_INPUT_SIZE];
     char* token_arr[MAX_TOKEN_ARRAY_SIZE + 1];  // MAX_TOKEN_ARRAY_SIZE plus the NULL at the end.
+    Command command;
+
     while (true) {
+        // reset command struct
+        command = (Command) {0};
+        
+        // getting and parsing user input 
         printf("mysh> ");
         get_input(input);
         get_tokens(token_arr, input);
+        if (get_command(&command, token_arr) != 0) {
+            continue;
+        };
+        
+        // exits if command is exit
+        if (strcmp(command.command, "exit") == 0) {
+            printf("Exiting mysh...");
+            break;
+        }
 
+        // outputs the content of the command struct
+        printf("command: %s\n", command.command);
+        
+        printf("args: ");
         int i = 0;
-        while (token_arr[i] != NULL) {
-            printf("%s ", token_arr[i]);
+        while (command.args[i] != NULL) {
+            printf("%s, ", command.args[i]);
             i++;
         }
         printf("\n");
 
+        printf("input-file: %s\n", command.input_file);
+        printf("output-file: %s\n", command.output_file);
+        printf("append? : %d\n", command.append);
+        printf("background? : %d\n", command.background);  
+        printf("\n");  
     }
 
     return 0;
--- lab_1/parser.c
+++ lab_1/parser.c
@@ -1,7 +1,23 @@
 #include "mysh.h"
 
 /**
- * gets user input from terminal.
+ * checks if a string is in an array.
+ * @param array takes in a string array to check.
+ * @param size takes in the size of the array to check.
+ * @param string takes in the string to find in the array.
+ * @return returns true if it FOUND the string in the array; returns false if NOT FOUND.
+ */
+bool contains_string(char* array[], int size, char* string) {
+    for (int i = 0; i < size; i++) {
+        if (strcmp(array[i], string) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * Gets user input from terminal.
  * @param input takes in a character array to store the input.
  * @return returns 0 if SUCCESS; returns 1 if an ERROR has occured.
  */
@@ -15,8 +31,8 @@ int get_input(char* input) {
 }
 
 /**
- * turns the input string to a list of tokens.
- * @param dest takes in an array of strings to store all the tokens there. 
+ * Turns the input string to a list of tokens.
+ * @param dest takes in an the address of the array of strings to store all the tokens there. 
  * @param input string input of user.
  * @return returns 0 if SUCCESS; returns 1 if an ERROR has occured.
  */
@@ -39,3 +55,111 @@ int get_tokens(char** dest, char* input) {
 
     return 0;
 }
+
+
+/**
+ * Sets the command struct to be executed given a list of tokens. 
+ * 
+ * Note: 
+ *  All commands follow the following format for parsing with an example:
+ *      command_name    command_args    redirect_operations(<, >, >>)   background_operation(&)
+ *      ls              -la             > file.txt                      &
+ *  
+ * @param command takes in the address of the Command struct and stores values there.   
+ * @param tokens takes in an array of tokens.
+ * @return returns 0 if SUCCESS; returns 1 if an ERROR has occured.
+ */
+int get_command(Command* command, char** tokens) {        
+    
+    /* These are special symbols for redirection and 
+        for declaring background processes */
+    char* INPUT_REDIRECTION_SYMBOL = "<";
+    char* OUTPUT_REDIRECTION_SYMBOL = ">";
+    char* APPEND_REDIRECTION_SYMBOL = ">>";
+    char* BACKGROUND_INDICATOR_SYMBOL = "&";
+    char* special_symbols[] = {
+        INPUT_REDIRECTION_SYMBOL, 
+        OUTPUT_REDIRECTION_SYMBOL, 
+        APPEND_REDIRECTION_SYMBOL, 
+        BACKGROUND_INDICATOR_SYMBOL
+    };
+    
+
+    int i = 0;
+    
+    /* Handles finding all command arguments until we find NULL 
+        or a special symbol (>, >>, <, or &). */
+    while (tokens[i] != NULL && !contains_string(special_symbols, sizeof(special_symbols)/sizeof(char*), tokens[i])) {
+        command->args[i] = tokens[i];
+        i++;
+    }
+    command->command = command->args[0];
+    command->args[i] = NULL;
+    
+
+    /* Handles redirection and background processes. 
+        Note:
+        [1] For redirection, there shall be only one input and 
+            output redirection (you cannot have two or more input and
+            output/append redirections.), or it will lead to an ERROR. 
+            
+        [2] Extra arguments or NULL found after redirections will lead to an ERROR.
+            Example of extra args in input: 
+                echo "hello" > output.txt EXTRA_ARGS
+
+            Redirection symbols (<, >, >>) must always be followed by only one file.
+        
+        [3] Background operation is optional, however, it must be the last thing in the 
+            user input. If not, then it will lead to an ERROR.
+            Example:
+                sleep 20 &
+
+        [3] Order of input and output redirection does not matter.
+    */
+    while (tokens[i] != NULL) {
+        if (strcmp(tokens[i], INPUT_REDIRECTION_SYMBOL) == 0) {
+            if (command->input_file != NULL) {
+                fprintf(stderr, "Error: Input redirection (<) has already been declared.\n");
+                return 1;
+            } else if (tokens[i + 1] == NULL) {
+                fprintf(stderr, "Error: Input file for redirection is not found.\n");
+                return 1;
+            } else {
+                command->input_file = tokens[i + 1];
+                i += 2;
+            }
+        } else if (strcmp(tokens[i], OUTPUT_REDIRECTION_SYMBOL) == 0) {
+            if (command->output_file != NULL) {
+                fprintf(stderr, "Error: Output/Append redirection (>, >>) has already been declared.\n");
+                return 1;
+            } else if (tokens[i + 1] == NULL) {
+                fprintf(stderr, "Error: Output file for redirection is not found.\n");
+                return 1;
+            } else {
+                command->output_file = tokens[i + 1];
+                i += 2;
+            }
+        } else if (strcmp(tokens[i], APPEND_REDIRECTION_SYMBOL) == 0) {
+            if (command->output_file != NULL) {
+                fprintf(stderr, "Error: Output/Append redirection (>, >>) has already been declared.\n");
+                return 1;
+            } else if (tokens[i + 1] == NULL) {
+                fprintf(stderr, "Error: Append file for redirection is not found.\n");
+                return 1;
+            } else {
+                command->output_file = tokens[i + 1];
+                command->append = true;
+                i += 2;
+            }
+        } else if (strcmp(tokens[i], BACKGROUND_INDICATOR_SYMBOL) == 0) {
+            command->background = true;
+            i++;
+        } else {
+            fprintf(stderr, "Error: Other arguments found after redirections (<, >, >>) or background process symbol (&).\n");
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
