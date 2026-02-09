fce93511669666284cb0ba4e136b6ca49030a1f5
choco-melk <hullezachristianjave@gmail.com>
2026-02-10 00:06:23 +0800

    feat: added user input

--- lab_1/mysh.h
+++ lab_1/mysh.h
@@ -1,12 +1,23 @@
 #ifndef MYSH_H
 #define MYSH_H
 
-#define MAX_NUM 1024
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
+
+#define MAX_TOKEN_ARRAY_SIZE 256
+#define MAX_INPUT_SIZE 1024
+
+typedef struct {
+    char *command;       
+    char *args[MAX_TOKEN_ARRAY_SIZE];     
+    char *input_file;     
+    char *output_file;   
+    bool append;         
+    bool background;     
+} Command;
 
 
 #endif
