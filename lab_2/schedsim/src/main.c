27b78363126120ae3a6c6428c0d81774454ad307
choco-melk <hullezachristianjave@gmail.com>
2026-03-07 14:22:32 +0800

    chore: added Makefile and gitignore

---
+++ lab_2/schedsim/src/main.c
@@ -0,0 +1,13 @@
+#include "../include/gantt.h"
+#include "../include/metrics.h"
+#include "../include/process.h"
+#include "../include/scheduler.h"
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
+
+
+
