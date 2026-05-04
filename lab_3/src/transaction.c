e80934692d2991b34305f9ab9072b846df6616bd
Kenneth Mondejar <mondejarkenneth64@gmail.com>
2026-05-04 08:24:42 +0800

    refactor: applying the bounded buffer pool loading/unloading into transaction

--- lab_3/src/transaction.c
+++ lab_3/src/transaction.c
@@ -3,6 +3,7 @@
 #include "bank.h"
 #include "timer.h"
 #include <stdio.h>
+#include "buffer_pool.h"
 
 void* execute_transaction(void* arg) {
     Transaction* tx = (Transaction*)arg;
@@ -17,26 +18,45 @@ void* execute_transaction(void* arg) {
         
         switch (op->type) {
             case OP_DEPOSIT:
+                load_account(&shared_pool, op->account_id);
                 deposit(op->account_id, op->amount_centavos);
+                unload_account(&shared_pool, op->account_id);
                 break;
                 
             case OP_WITHDRAW:
+                load_account(&shared_pool, op->account_id);
                 if (!withdraw(op->account_id, op->amount_centavos)) {
+                    unload_account(&shared_pool, op->account_id);
                     tx->status = TX_ABORTED;
                     return NULL;
                 }
+                unload_account(&shared_pool, op->account_id);
                 break;
                 
             case OP_TRANSFER:
+                // Buffer Pool Deadlock Prevention(consistent order)
+                int first = (op->account_id < op->target_account) ? op->account_id : op->target_account;
+                int second = (op->account_id < op->target_account) ? op->target_account : op->account_id;
+
+                load_account(&shared_pool, first);
+                load_account(&shared_pool, second);
+
                 if (!transfer(op->account_id, op->target_account, op->amount_centavos, tx->tx_id)) {
+                    unload_account(&shared_pool, first);
+                    unload_account(&shared_pool, second);
                     tx->status = TX_ABORTED;
                     return NULL;
                 }
+
+                unload_account(&shared_pool, second);
+                unload_account(&shared_pool, first);
                 break;
                 
             case OP_BALANCE: {
+                load_account(&shared_pool, op->account_id);
                 int balance = get_balance(op->account_id);
                 printf("T%d: Account %d balance = PHP %d.%02d\n", tx->tx_id, op->account_id, balance / 100, balance % 100);
+                unload_account(&shared_pool, op->account_id);
                 break;
             }
         }
